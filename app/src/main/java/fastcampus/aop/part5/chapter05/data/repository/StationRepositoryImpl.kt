99d69b8dafedfc77c227a24148a5092ed37cfc57
devussy <seungyong.yun@styleshare.kr>
2021-05-23 11:12:07 +0900

    지하철 상세 정보 가져오기

--- app/src/main/java/fastcampus/aop/part5/chapter05/data/repository/StationRepositoryImpl.kt
+++ app/src/main/java/fastcampus/aop/part5/chapter05/data/repository/StationRepositoryImpl.kt
@@ -1,9 +1,12 @@
 package fastcampus.aop.part5.chapter05.data.repository
 
 import fastcampus.aop.part5.chapter05.data.api.StationApi
+import fastcampus.aop.part5.chapter05.data.api.StationArrivalsApi
+import fastcampus.aop.part5.chapter05.data.api.response.mapper.toArrivalInformation
 import fastcampus.aop.part5.chapter05.data.db.StationDao
 import fastcampus.aop.part5.chapter05.data.db.entity.mapper.toStations
 import fastcampus.aop.part5.chapter05.data.preference.PreferenceManager
+import fastcampus.aop.part5.chapter05.domain.ArrivalInformation
 import fastcampus.aop.part5.chapter05.domain.Station
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
@@ -13,6 +16,7 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 
 class StationRepositoryImpl(
+    private val stationArrivalsApi: StationArrivalsApi,
     private val stationApi: StationApi,
     private val stationDao: StationDao,
     private val preferenceManager: PreferenceManager,
@@ -33,6 +37,16 @@ class StationRepositoryImpl(
             stationDao.insertStationSubways(stationApi.getStationSubways())
             preferenceManager.putLong(KEY_LAST_DATABASE_UPDATED_TIME_MILLIS, fileUpdatedTimeMillis)
         }
+    }
+
+    override suspend fun getStationArrivals(stationName: String): List<ArrivalInformation> = withContext(dispatcher) {
+        stationArrivalsApi.getRealtimeStationArrivals(stationName)
+            .body()
+            ?.realtimeArrivalList
+            ?.toArrivalInformation()
+            ?.distinctBy { it.direction }
+            ?.sortedBy { it.subway }
+            ?: throw RuntimeException("도착 정보를 불러오는 데에 실패했습니다.")
     }
 
     companion object {
